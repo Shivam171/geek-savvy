166fa6196cc88736de08dac4459bf23ed3a282ab
shivam171 <shiv.op@gmail.com>
2024-06-19 21:13:48 +0530

    Create signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -27,6 +28,50 @@ export const signup = async (req, res, next) => {
     // Save user to database
     await newUser.save();
     res.json("Signup successful");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check for missing fields
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required!"));
+  }
+
+  try {
+    // Find user in database
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid credentials"));
+    }
+
+    // Check if password is correct
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid credentials"));
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    // Remove password from response
+    const { password: pass, ...rest } = validUser._doc;
+
+    // Set JWT token in cookie
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (err) {
     next(err);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
 const router = express.Router();
+
+import { signup } from "../controllers/auth.controller.js";
 router.post("/signup", signup);
+
+import { signin } from "../controllers/auth.controller.js";
+router.post("/signin", signin);
+
 export default router;
