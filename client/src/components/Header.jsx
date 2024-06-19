1ec031241ca266dcc6bfc5e5c44c3420f1445ea8
shivam171 <shiv.op@gmail.com>
2024-06-19 12:51:32 +0530

    Updated Repo Name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function HeaderComponent() {
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">Shivam&apos;s</span>Blog
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">Geek</span>Savvy
       </Link>
       <form>
         <TextInput
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto">
+        {/* left */}
+        <div className="">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">Geek</span>Savvy
+          </Link>
+          <p className="text-sm mt-5">Sign up with your email and password or with Google.</p>
+        </div>
+        {/* right */}
+        <div className="">
+
+        </div>
+      </div>
+    </div>
   )
 }
