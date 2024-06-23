769b66849e55f95da3cc34ebdebfdcdd4bdee2a9
shivam171 <shiv.op@gmail.com>
2024-06-23 20:38:31 +0530

    Added delete user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -61,3 +61,16 @@ export const updateUser = async (req, res, next) => {
     next(err);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this account!"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted!");
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,4 +9,7 @@ import { updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 router.put("/update/:userId", verifyToken, updateUser);
 
+import { deleteUser } from "../controllers/user.controller.js";
+router.delete("/delete/:userId", verifyToken, deleteUser);
+
 export default router;
